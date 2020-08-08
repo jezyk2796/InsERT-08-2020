93c53a280920428d188feacebc3305385469635c
Szymon <jezyk2796@op.pl>
2020-08-08 19:34:07 +0200

    basic style added to elements. Getting date and time from API created

--- src/app/weather-widget-main/weather-widget-main.component.ts
+++ src/app/weather-widget-main/weather-widget-main.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherWidgetMainComponent implements OnInit {
 
-  constructor() { }
+  apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=wroclaw&appid=945a05b7b60aff343b86c6ec33f4afd3';
+  date;
 
   ngOnInit() {
+    this.getWeatherData();
   }
 
+  getWeatherData() {
+    fetch(this.apiCall)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // to delete
+
+        this.date = data.dt * 1000;
+        this.getPartOfDay(data.dt);
+      });
+  }
+
+  getPartOfDay(time) {
+    const currentDate = new Date(time * 1000);
+    const currentHour = currentDate.getHours();
+    if (currentHour >= 5 && currentHour <= 11) {
+      return 'morning';
+    } else if (currentHour >= 12 && currentHour <= 22) {
+      return 'afternoon';
+    } else {
+      return 'night';
+    }
+  }
 }
