a45e258746e4f0db4502fb5720de7f38311d7cc2
Szymon <jezyk2796@op.pl>
2020-08-08 12:06:15 +0200

    initial commit. Created environment and main component

---
+++ src/app/weather-widget-main/weather-widget-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget-main',
+  templateUrl: './weather-widget-main.component.html',
+  styleUrls: ['./weather-widget-main.component.scss']
+})
+export class WeatherWidgetMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
